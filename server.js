8efd29b3c1eb659b29a9ccf7be0548b4bc10baf1
Yuvraj Singh <syuvraj1900@gmail.com>
2022-09-16 16:31:02 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Yuvraj singh Student ID:155580210 Date: 16/09/2022
+*
+*  Online (Cyclic) URL: _______________________________________________________
+*
+********************************************************************************/ 
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Yuvraj Singh ID:155580210");
 });
 
 // setup http server to listen on HTTP_PORT
