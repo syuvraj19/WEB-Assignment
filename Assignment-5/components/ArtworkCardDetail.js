15e21b69ac7bfd38043f1f5e7f55dc0867f19a39
Yuvraj Singh <99282818+syuvraj19@users.noreply.github.com>
2023-11-26 16:02:56 -0500

    Add files via upload

---
+++ Assignment-5/components/ArtworkCardDetail.js
@@ -0,0 +1,104 @@
+/** @format */
+
+import useSWR from "swr";
+import Error from "next/error";
+import Card from "react-bootstrap/Card";
+import { favouritesAtom } from "@/store";
+import { useAtom } from "jotai";
+import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+
+// const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function ArtworkCardDetail(props) {
+  const url = `https://collectionapi.metmuseum.org/public/collection/v1/objects`;
+
+  const [favouritesList, setFavouritesList] = useAtom(favouritesAtom);
+  const [showAdded, setShowAdded] = useState();
+  useEffect(() => {
+    if (favouritesList.includes(props.objectID) == true) {
+      setShowAdded(true);
+    } else {
+      setShowAdded(false);
+    }
+  });
+
+  function favouriteClicked() {
+    if (showAdded == true) {
+      setFavouritesList((current) =>
+        current.filter((fav) => fav != props.objectID)
+      );
+      setShowAdded(false);
+    } else {
+      setFavouritesList((current) => [...current, props.objectID]);
+      setShowAdded(true);
+    }
+  }
+
+  // const { data, error } = useSWR(url, fetcher);
+  const { data, error } = useSWR(
+    props.objectID ? `${url}/${props.objectID}` : null
+  );
+  if (error) {
+    return <Error statusCode={404} />;
+  }
+  if (data) {
+    return (
+      <>
+        <Card>
+          {data?.primaryImage && (
+            <Card.Img variant="top" src={data?.primaryImage} />
+          )}
+          <Card.Body>
+            <Card.Title>{data?.title}</Card.Title>
+            <Card.Text>
+              <strong>Date: </strong>
+              {data?.objectDate ? data.objectDate : "N/A"} <br />
+              <strong>Classification: </strong>
+              {data?.classification ? data?.classification : "N/A"}
+              <br />
+              <strong>Medium: </strong>
+              {data?.medium ? data?.medium : "N/A"}
+              <br />
+              <br />
+              <strong>Artist: </strong>
+              {data?.artistDisplayName ? (
+                <span>
+                  {data.artistDisplayName}
+                  {data.artistWikidata_URL ? (
+                    <span>
+                      {" ( "}
+                      <a
+                        href={data.artistWikidata_URL}
+                        target="_blank"
+                        rel="noreferrer">
+                        wiki
+                      </a>
+                      {" )"}
+                    </span>
+                  ) : null}
+                </span>
+              ) : (
+                "N/A"
+              )}
+              <br />
+              <strong>Credit Line: </strong>
+              {data?.creditLine ? data?.creditLine : "N/A"}
+              <br />
+              <strong>Dimensions: </strong>
+              {data?.dimensions ? data?.dimensions : "N/A"}
+              &nbsp; &nbsp;
+              <br />
+              <br />
+              <Button
+                variant={showAdded == true ? "primary" : "outline-primary"}
+                onClick={favouriteClicked}>
+                {showAdded == true ? "+ Favourite (added)" : "+ Favourite"}
+              </Button>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </>
+    );
+  } else return null;
+}
---
+++ Assignment-5/store.js
@@ -0,0 +1,6 @@
+/** @format */
+
+import { atom } from "jotai";
+
+export const favouritesAtom = atom([]);
+export const searchHistoryAtom = atom([]);
