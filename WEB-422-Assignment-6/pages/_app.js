888eebeecda51dc9c26d9325f630f2b2d72572cc
Yuvraj Singh <syuvraj1900@gmail.com>
2024-07-21 20:54:09 +0530

    Final cmmit

---
+++ WEB-422-Assignment-6/pages/_app.js
@@ -0,0 +1,29 @@
+import Layout from "@/components/Layout";
+import RouteGuard from "@/components/RouteGuard";
+import "@/styles/bootstrap.min.css";
+import { SWRConfig } from "swr";
+
+
+export default function App({ Component, pageProps }) {
+  return (
+    <RouteGuard>
+      <Layout>
+        <SWRConfig
+          value={{
+            fetcher: async (url) => {
+              const res = await fetch(url);
+              if (!res.ok) {
+                const error = new Error(
+                  "An error occurred while fetching the data."
+                );
+                throw error;
+              }
+              return res.json();
+            },
+          }}>
+          <Component {...pageProps} />
+        </SWRConfig>
+      </Layout>
+    </RouteGuard>
+  );
+}
---
+++ WEB-422-Assignment-6/pages/artwork/[objectID].js
@@ -0,0 +1,17 @@
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import ArtworkCardDetail from "@/components/ArtworkCardDetail";
+import { useRouter } from "next/router";
+
+
+export default function ObjectID() {
+  const router = useRouter();
+  const { objectID } = router.query;
+  return (
+    <Row>
+      <Col>
+        <ArtworkCardDetail objectID={objectID} />
+      </Col>
+    </Row>
+  );
+}
