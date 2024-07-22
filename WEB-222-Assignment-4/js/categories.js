4efa8b9aafced215313ce7b3d8a6aa1080601649
Yuvraj Singh <syuvraj1900@gmail.com>
2024-07-22 21:10:45 +0530

    Final commit

---
+++ WEB-222-Assignment-4/js/categories.js
@@ -0,0 +1,32 @@
+/**
+ * categories.js
+ *
+ * The store's categories are defined in an Array of category Objects. Each
+ * category Object has the following properties:
+ *
+ *  - id: String, a unique category id (e.g., "c1", "c2", "c3")
+ *  - name: String, the human-readable name for the category (e.g., "Perfume")
+ */
+
+window.categories = [
+  {
+    id: "cat1",
+    type: "Highs"
+  },
+  {
+    id: "cat2",
+    type: "Mids"
+  },
+  {
+    id: "cat3",
+    type: "Lows"
+  },
+  {
+    id: "cat4",
+    type: "Slides"
+  },
+  {
+    id: "cat5",
+    type: "Luxury"
+  }
+];
